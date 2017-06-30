0dbe3183fbce46a9629204ebbb9eea14396dca3c
Rehan Ahmed <Rehan.ahmed19@outlook.com>
2017-06-30 22:28:19 +0500

    Add socket.io on client and server

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ let express = require('express');
 let app = express();
 
 let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+io.on('connection', () => {
+	console.log('User connected via socket.io!');
+});
+
 http.listen(PORT, function(){
 	console.log('Server started');
-})
+});
