2144b2c7150bfb02537266e1c6eb993d5b80b9c6
Rehan Ahmed <Rehan.ahmed19@outlook.com>
2017-06-30 21:45:29 +0500

    Setup basic server

---
+++ server.js
@@ -0,0 +1,11 @@
+let PORT = process.env.PORT || 3000;
+let express = require('express');
+let app = express();
+
+let http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(PORT, function(){
+	console.log('Server started');
+})
